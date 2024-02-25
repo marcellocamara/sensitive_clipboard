e5d13e1182d4f56cd0282279b41bc96b6739b1a8
Marcello Câmara <marcellocamara@id.uff.br>
2024-02-25 12:12:28 -0300

    feat: update example app with new migration guide https://docs.flutter.dev/release/breaking-changes/flutter-gradle-plugin-apply

--- example/android/app/src/main/kotlin/marcello/dev/sensitive_clipboard_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/marcello/dev/sensitive_clipboard_example/MainActivity.kt
@@ -2,5 +2,4 @@ package marcello.dev.sensitive_clipboard_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
